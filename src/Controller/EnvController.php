4518e83660ff350c1f72e936215b0f0bd4f09654
saysa <sbo@conamed.de>
2018-09-18 12:56:02 +0200

    env test

--- src/Controller/EnvController.php
+++ src/Controller/EnvController.php
@@ -3,17 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EnvController extends AbstractController
 {
+    private $param;
+
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->param = $parameterBag;
+    }
+
     /**
      * @Route("/env", name="env")
      */
     public function index()
     {
+        $prenom = $this->param->get('prenom');
+
         return $this->render('env/index.html.twig', [
-            'controller_name' => 'EnvController',
+            'controller_name' => $prenom,
         ]);
     }
 }
